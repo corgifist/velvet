f4db0d11590eaeb2a72973de120ed430002905ed
corgifist <sxuare.science@gmail.com>
2026-05-31 20:20:22 +0300

    added separate `include/` folder

---
+++ include/velvet.h
@@ -0,0 +1,6 @@
+#ifndef VELVET_H
+#define VELVET_H
+
+#define M_PI 3.14
+
+#endif // VELVET_H
--- src/main.c
+++ src/main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#include "velvet.h"
+
 int main(int argc, const char *argv[]) {
     char cwd[256];
     getcwd(cwd, 256);
     printf("%s | %s\n", argv[0], cwd);
+    printf("%0.2f\n", M_PI);
     return 0;
 }
