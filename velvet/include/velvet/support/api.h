3a2355a29887f1b608238d9665d129306fb71e1a
corgifist <sxuare.science@gmail.com>
2026-07-08 01:22:27 +0300

    made macro naming more consistent

--- velvet/include/velvet/support/api.h
+++ velvet/include/velvet/support/api.h
@@ -4,17 +4,17 @@
 #include "platform.h"
 
 #if VL_PLATFORM(WINDOWS)
-    #ifdef VELVET_EXPORT_SYMBOLS
+    #ifdef VL_EXPORT_SYMBOLS
         #define VL_API __declspec(dllexport)
     #else
         #define VL_API __declspec(dllimport)
-    #endif // VELET_EXPORT_SYMBOLS
+    #endif // VL_EXPORT_SYMBOLS
 #else // unix-like systems
-    #ifdef VELVET_EXPORT_SYMBOLS
+    #ifdef VL_EXPORT_SYMBOLS
         #define VL_API __attribute__((visibility("default")))
     #else
         #define VL_API
-    #endif // VELVET_EXPORT_SYMBOLS
+    #endif // VL_EXPORT_SYMBOLS
 #endif
 
 #define VL_VA_NARGS_(A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16, COUNT, ...) COUNT
